a22c91a021e3955f48f2f8dcf1772815430248df
Mithaniel Villard <mvillardhernandez@gaming.tech>
2026-03-02 22:40:17 +0100

    [ADD] Added Binding struct to simplify proxy binding

--- Foundry/include/Node.h
+++ Foundry/include/Node.h
@@ -4,6 +4,7 @@
 #include "Debug.h"
 #include "Define.h"
 #include "Event.hpp"
+#include "SceneTree.h"
 #include "Scripting/Lua/LuaScriptInstance.hpp"
 
 #include <functional>
@@ -50,7 +51,7 @@ public:
 
 	Node& GetChild(uint32 index);
 	std::vector<std::reference_wrapper<Node>> GetChildren();
-	int32 GetChildCount();
+	uint32 GetChildCount();
 
 	template <NodeType T>
 	T& GetNode(std::string const& path);
@@ -58,7 +59,7 @@ public:
 	//Only destroy the node if it has a parent
 	void Destroy();
 	virtual void Reparent(Node& newParent, bool keepGlobalTransform = true);
-	void MoveChild(Node const& child, int32 to);
+	void MoveChild(Node const& child, uint32 to);
 
 	//override this method if the inherited node is not trivially copyable
 	virtual std::unique_ptr<Node> Clone();
