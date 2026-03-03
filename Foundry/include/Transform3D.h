4bb6bc90512d27b581862b3c6ec8fd3b0df46389
Clement-Trt <ctouratier@gaming.tech>
2026-03-03 10:31:45 +0100

    [ADD] Transform3D

---
+++ Foundry/include/Transform3D.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+
+class Transform3D
+{
+	glm::vec3 m_position;
+	glm::vec3 m_rotation;
+	glm::vec3 m_scale;
+
+	glm::vec3 m_right;
+	glm::vec3 m_up;
+	glm::vec3 m_forward;
+
+	glm::mat4 m_rotationMatrix;
+	glm::mat4 m_invRotationMatrix;
+	glm::mat4 m_Transform;
+	glm::mat4 m_invTransform;
+
+	bool m_isDirty;
+	bool m_isRotationDirty;
+	bool m_isInvDirty;
+
+	void UpdateRotationMatrix();
+	void UpdateTransform();
+	void UpdateInvTransform();
+public:
+
+	Transform3D();
+
+	const glm::vec3& GetPosition() const;
+	float GetX() const;
+	float GetY() const;
+	float GetZ() const;
+	const glm::mat4& GetMatrixRotation();
+	const glm::mat4& GetInverseMatrixRotation();
+	const glm::vec3& GetRotation() const;
+	float GetYaw() const;
+	float GetPitch() const;
+	float GetRoll() const;
+	float GetMaxScale() const;
+	float GetMinScale() const;
+	const glm::vec3& GetScale() const;
+	const glm::vec3& GetRight();
+	const glm::vec3& GetUp();
+	const glm::vec3& GetForward();
+
+	glm::mat4& GetMatrix();
+	glm::mat4& GetInvMatrix();
+
+	bool GetDirty() { return mIsDirty; }
+
+	void SetPosition(glm::vec3 pos);
+	void SetX(float x);
+	void SetY(float y);
+	void SetZ(float z);
+	void SetRotation(glm::vec3 rot);
+	void SetYaw(float yaw);
+	void SetPitch(float pitch);
+	void SetRoll(float roll);
+	void SetScale(glm::vec3 scale);
+
+	void AddPosition(glm::vec3 pos);
+	void AddX(float x);
+	void AddY(float y);
+	void AddZ(float z);
+	void AddRotation(glm::vec3 rot);
+	void AddYaw(float yaw);
+	void AddPitch(float pitch);
+	void AddRoll(float roll);
+	void AddScale(glm::vec3 scale);
+
+	void ApplyTransform(Transform3D& Transform3D);
+	void Update();
+};
---
+++ Foundry/src/Transform3D.cpp
@@ -0,0 +1,421 @@
+﻿#include "Transform3D.h"
+
+Transform3D::Transform3D() :
+	m_position(0, 0, 0),
+	m_scale(1, 1, 1),
+	m_rotation(0, 0, 0),
+	m_isDirty(true),
+	m_isInvDirty(true),
+	m_pParent(nullptr)
+{
+
+}
+
+Transform3D::~Transform3D()
+{
+	delete m_pParent;
+}
+
+void Transform3D::UpdateTransform()
+{
+	if (m_isDirty == false) return;
+
+	if (m_pParent)
+	{
+		m_pParent->UpdateTransform();
+		m_pParent->UpdateRotationMatrix();
+	}
+
+	glm::mat4 T = Translate(m_position);
+	glm::mat4 S = Scale(m_scale);
+
+	m_transform = T * m_rotationMatrix * S;
+	m_isDirty = false;
+}
+
+void Transform3D::UpdateRotationMatrix()
+{
+	if (m_isRotationDirty == false) return;
+
+	m_rotationMatrix = RotateYawPitchRoll(m_rotation);
+	m_invRotationMatrix = glm::inverse(m_rotationMatrix);
+
+	m_right = { m_rotationMatrix[0][0], m_rotationMatrix[0][1], m_rotationMatrix[0][2] };
+	m_up = { m_rotationMatrix[1][0], m_rotationMatrix[1][1], m_rotationMatrix[1][2] };
+	m_forward = { m_rotationMatrix[2][0], m_rotationMatrix[2][1], m_rotationMatrix[2][2] };
+
+	m_isRotationDirty = false;
+}
+
+void Transform3D::UpdateInvTransform()
+{
+	if (m_isInvDirty == false) return;
+
+	m_invTransform = glm::inverse(m_transform);
+	m_isInvDirty = false;
+}
+
+const glm::vec3& Transform3D::GetPosition() const
+{
+	return m_position;
+}
+
+float Transform3D::GetX() const
+{
+	return m_position.x;
+}
+
+float Transform3D::GetY() const
+{
+	return m_position.y;
+}
+
+float Transform3D::GetZ() const
+{
+	return m_position.z;
+}
+
+const glm::mat4& Transform3D::GetMatrixRotation()
+{
+	UpdateRotationMatrix();
+	return m_rotationMatrix;
+}
+
+const glm::mat4& Transform3D::GetInverseMatrixRotation()
+{
+	UpdateRotationMatrix();
+	return m_invRotationMatrix;
+}
+
+const glm::vec3& Transform3D::GetRotation() const
+{
+	return m_rotation;
+}
+
+float Transform3D::GetYaw() const
+{
+	return m_rotation.x;
+}
+
+float Transform3D::GetPitch() const
+... (213lignes restantes)
+
+message.txt
+6 Ko
+﻿
+#include "NewTransform.h"
+
+Transform3D::Transform3D() :
+	m_position(0, 0, 0),
+	m_scale(1, 1, 1),
+	m_rotation(0, 0, 0),
+	m_isDirty(true),
+	m_isInvDirty(true),
+	m_pParent(nullptr)
+{
+
+}
+
+Transform3D::~Transform3D()
+{
+	delete m_pParent;
+}
+
+void Transform3D::UpdateTransform()
+{
+	if (m_isDirty == false) return;
+
+	if (m_pParent)
+	{
+		m_pParent->UpdateTransform();
+		m_pParent->UpdateRotationMatrix();
+	}
+
+	glm::mat4 T = Translate(m_position);
+	glm::mat4 S = Scale(m_scale);
+
+	m_transform = T * m_rotationMatrix * S;
+	m_isDirty = false;
+}
+
+void Transform3D::UpdateRotationMatrix()
+{
+	if (m_isRotationDirty == false) return;
+
+	m_rotationMatrix = RotateYawPitchRoll(m_rotation);
+	m_invRotationMatrix = glm::inverse(m_rotationMatrix);
+
+	m_right = { m_rotationMatrix[0][0], m_rotationMatrix[0][1], m_rotationMatrix[0][2] };
+	m_up = { m_rotationMatrix[1][0], m_rotationMatrix[1][1], m_rotationMatrix[1][2] };
+	m_forward = { m_rotationMatrix[2][0], m_rotationMatrix[2][1], m_rotationMatrix[2][2] };
+
+	m_isRotationDirty = false;
+}
+
+void Transform3D::UpdateInvTransform()
+{
+	if (m_isInvDirty == false) return;
+
+	m_invTransform = glm::inverse(m_transform);
+	m_isInvDirty = false;
+}
+
+const glm::vec3& Transform3D::GetPosition() const
+{
+	return m_position;
+}
+
+float Transform3D::GetX() const
+{
+	return m_position.x;
+}
+
+float Transform3D::GetY() const
+{
+	return m_position.y;
+}
+
+float Transform3D::GetZ() const
+{
+	return m_position.z;
+}
+
+const glm::mat4& Transform3D::GetMatrixRotation()
+{
+	UpdateRotationMatrix();
+	return m_rotationMatrix;
+}
+
+const glm::mat4& Transform3D::GetInverseMatrixRotation()
+{
+	UpdateRotationMatrix();
+	return m_invRotationMatrix;
+}
+
+const glm::vec3& Transform3D::GetRotation() const
+{
+	return m_rotation;
+}
+
+float Transform3D::GetYaw() const
+{
+	return m_rotation.x;
+}
+
+float Transform3D::GetPitch() const
+{
+	return m_rotation.y;
+}
+
+float Transform3D::GetRoll() const
+{
+	return m_rotation.z;
+}
+
+float Transform3D::GetMaxScale() const
+{
+	return std::max(m_scale.x, std::max(m_scale.y, m_scale.z));
+}
+
+float Transform3D::GetMinScale() const
+{
+	return std::min(m_scale.x, std::min(m_scale.y, m_scale.z));
+}
+
+const glm::vec3& Transform3D::GetScale() const
+{
+	return m_scale;
+}
+
+const glm::vec3& Transform3D::GetUp()
+{
+	UpdateRotationMatrix();
+	return m_up;
+}
+
+const glm::vec3& Transform3D::GetRight()
+{
+	UpdateRotationMatrix();
+	return m_right;
+}
+
+const glm::vec3& Transform3D::GetForward()
+{
+	UpdateRotationMatrix();
+	return m_forward;
+}
+
+glm::mat4& Transform3D::GetMatrix()
+{
+	UpdateRotationMatrix();
+	UpdateTransform();
+	return m_transform;
+}
+
+glm::mat4& Transform3D::GetInvMatrix()
+{
+	UpdateRotationMatrix();
+	UpdateTransform();
+	UpdateInvTransform();
+	return m_transform;
+}
+
+Transform3D* Transform3D::GetParent()
+{
+	return m_pParent;
+}
+
+void Transform3D::SetParent(Transform3D * parent)
+{
+	m_pParent = parent;
+}
+
+void Transform3D::SetChild(Transform3D * parent)
+{
+	parent->SetParent(this);
+}
+
+void Transform3D::SetPosition(glm::vec3 pos)
+{
+	m_position = pos;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::SetX(float x)
+{
+	m_position.x = x;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::SetY(float y)
+{
+	m_position.y = y;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::SetZ(float z)
+{
+	m_position.z = z;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::SetRotation(glm::vec3 rot)
+{
+	m_rotation = rot;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::SetYaw(float yaw)
+{
+	m_rotation.y = yaw;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::SetPitch(float pitch)
+{
+	m_rotation.x = pitch;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::SetRoll(float roll)
+{
+	m_rotation.z = roll;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::SetScale(glm::vec3 scale)
+{
+	m_scale = scale;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::AddPosition(glm::vec3 pos)
+{
+	m_position += pos;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::AddX(float x)
+{
+	m_position.x += x;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::AddY(float y)
+{
+	m_position.y += y;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::AddZ(float z)
+{
+	m_position.z += z;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::AddRotation(glm::vec3 rot)
+{
+	m_rotation += rot;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::AddYaw(float yaw)
+{
+	m_rotation.x += yaw;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::AddPitch(float pitch)
+{
+	m_rotation.y += pitch;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::AddRoll(float roll)
+{
+	m_rotation.z += roll;
+	m_isDirty = true;
+	m_isInvDirty = true;
+	m_isRotationDirty = true;
+}
+
+void Transform3D::AddScale(glm::vec3 scale)
+{
+	m_scale += scale;
+	m_isDirty = true;
+	m_isInvDirty = true;
+}
+
+void Transform3D::ApplyTransform(Transform3D & transform)
+{
+	m_rotationMatrix = transform.GetMatrixRotation() * GetMatrixRotation();
+	m_transform = transform.GetMatrix() * GetMatrix();
+	m_isInvDirty = true;
+}
+
+void Transform3D::Update()
+{
+}
