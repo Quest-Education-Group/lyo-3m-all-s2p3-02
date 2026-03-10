16dbace2458b56dbefa337f0a7350ef07fa7c68c
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2026-03-10 16:48:01 +0100

    [ADD] Error handling

--- Ore/include/EventManager.h
+++ Ore/include/EventManager.h
@@ -11,6 +11,21 @@ enum class GamepadId
 {
     GAMEPAD_1,
     GAMEPAD_2,
+    GAMEPAD_3,
+    GAMEPAD_4,
+    GAMEPAD_5,
+    GAMEPAD_6,
+    GAMEPAD_7,
+    GAMEPAD_8,
+    GAMEPAD_9,
+    GAMEPAD_10,
+    GAMEPAD_11,
+    GAMEPAD_12,
+    GAMEPAD_13,
+    GAMEPAD_14,
+    GAMEPAD_15,
+    GAMEPAD_16,
+
     GAMEPAD_MAX
 };
 
--- Ore/src/EventManager.cpp
+++ Ore/src/EventManager.cpp
@@ -7,6 +7,18 @@ Event<void(GamepadId)> EventManager::gamepadDisconnected = Event<void(GamepadId)
 bool EventManager::GetKey(Window window, EventInput key, EventAction event)
 {
 	int const scancode = glfwGetKeyScancode((int)key);
+
+    if(scancode == GLFW_NOT_INITIALIZED)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "GLFW is not initialized, did you create a window before calling the function ?");
+        return false;
+    }
+    if(scancode == GLFW_INVALID_ENUM)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "The value does not correspond to the enum");
+        return false;
+    }
+
 	const char* ch = glfwGetKeyName((int)key, scancode);
 	
 	int val = 0;
@@ -30,7 +42,20 @@ bool EventManager::GetKey(Window window, EventInput key, EventAction event)
 bool EventManager::GetMouseKey(Window window, EventInput key, EventAction event)
 {
 	int state = glfwGetMouseButton(window.Get(), (int)key);
-	if (state == (int)event)
+
+	if(state == GLFW_NOT_INITIALIZED)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "GLFW is not initialized, did you create a window before calling the function ?");
+        return false;
+    }
+
+    if(state == GLFW_INVALID_ENUM)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "The value does not correspond to the enum");
+        return false;
+    }
+
+    if (state == (int)event)
 	{
 		Logger::LogWithLevel(LogLevel::DEBUG, (int)key);
 		return true;
@@ -41,6 +66,19 @@ bool EventManager::GetMouseKey(Window window, EventInput key, EventAction event)
 bool EventManager::CheckGamepad(GamepadId id)
 {
 	int present = glfwJoystickPresent((int)id);
+
+    if(present == GLFW_NOT_INITIALIZED)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "GLFW is not initialized, did you create a window before calling the function ?");
+        return false;
+    }
+
+    if(present == GLFW_INVALID_ENUM)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "The value does not correspond to the enum");
+        return false;
+    }
+
 	Logger::LogWithLevel(LogLevel::DEBUG, present);
 	return present;
 }
@@ -56,6 +94,13 @@ void EventManager::JoystickCallback(int jId, int event)
 std::string_view EventManager::GetGamepadName(GamepadId id)
 {
 	const char* pName = glfwGetGamepadName((int)id);
+
+    if(pName == NULL)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "Couldn't get gamepad's name at id :", id, "use CheckGamepad first to check if it is present");
+        return "";
+    }
+
 	std::string_view gamepadName(pName);
 	Logger::LogWithLevel(LogLevel::DEBUG, gamepadName);
 	return gamepadName;
@@ -65,6 +110,13 @@ float EventManager::GetGamepadAxes(GamepadId id, EventInput key)
 {
 	int axesCount;
 	const float* pAxes = glfwGetJoystickAxes((int)id, &axesCount);
+
+    if(pAxes == NULL)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "Couldn't get joytick's axes at id :", id, "use CheckGamepad first to check if it is present");
+        return {};
+    }
+
 	Logger::LogWithLevel(LogLevel::DEBUG, pAxes[(int)key]);
 	return pAxes[(int)key];
 }
@@ -72,14 +124,18 @@ float EventManager::GetGamepadAxes(GamepadId id, EventInput key)
 bool EventManager::GetButton(GamepadId id, EventInput button)
 {
 	GLFWgamepadstate state;
+    int error = glfwGetGamepadState((int)id, &state);
 
-	if (glfwGetGamepadState((int)id, &state))
+    if(error == GLFW_FALSE)
+    {
+        Logger::LogWithLevel(LogLevel::ERROR, "Couldn't get gamepad's button at id :", id, "use CheckGamepad first to check if it is present");
+        return false;
+    }
+
+	if (state.buttons[(int)button])
 	{
-		if (state.buttons[(int)button])
-		{
-			Logger::LogWithLevel(LogLevel::DEBUG, (int)button);
-			return true;
-		}
+        Logger::LogWithLevel(LogLevel::DEBUG, (int)button);
+		return true;
 	}
     return false;
 }
