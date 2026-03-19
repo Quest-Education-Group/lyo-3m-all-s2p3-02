fadb13379fc824fef22d58db24c1e0e761689491
Elia Maillard-Salin <147042560+EliaMaillard-Salin@users.noreply.github.com>
2026-03-19 12:04:08 +0100

    [ADD] Empty AssetLoader Class

---
+++ Foundry/src/AssetLoading/AssetLoader.cpp
@@ -0,0 +1,12 @@
+#include "AssetLoader.h"
+
+uptr<SceneData> AssetLoader::LoadFile(std::string const& filePath)
+{
+	SceneData scene = {};
+	//	
+
+	// ParseMeshs
+	// ParseMaterials
+	
+	return SceneData();
+}
