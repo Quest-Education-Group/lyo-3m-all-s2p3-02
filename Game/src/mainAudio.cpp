3bf1b5f67244713ed63cb470d09b7712d812b02c
Paul-Henri-Solat-Gtech <psolat@gaming.tech>
2026-03-24 16:11:06 +0100

    Update mainAudio.cpp

--- Game/src/mainAudio.cpp
+++ Game/src/mainAudio.cpp
@@ -4,11 +4,11 @@
 int main()
 {
     auto audioEm = Node::CreateNode<NodeAudioEmitter>("AudioEmitter");
-    audioEm->AudioSetFile("C:/Users/psolat/Documents/GitHub/lyo-3m-all-s2p3-02/res/bass-wiggle.mp3"); // TEST ONLY (replace with your file)
+    audioEm->AudioSetFile("res/bass-wiggle.mp3"); // TEST ONLY (replace with your file)
     audioEm->AudioSetLoop(true);
 
     auto audioEm2 = Node::CreateNode<NodeAudioEmitter>("AudioEmitter2");
-    audioEm2->AudioSetFile("C:/Users/psolat/Documents/GitHub/lyo-3m-all-s2p3-02/res/applause.mp3"); // TEST ONLY (replace with your file)
+    audioEm2->AudioSetFile("res/applause.mp3"); // TEST ONLY (replace with your file)
     audioEm2->AudioSetLoop(true);
 
     audioEm->AudioPlay();
