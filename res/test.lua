fe4f99192f2853dcf35ca5007f76642159b1be05
Clement-Trt <ctouratier@gaming.tech>
2026-04-06 18:17:28 +0200

    .

--- res/test.lua
+++ res/test.lua
@@ -1,19 +1,41 @@
 
--- ---@type Node3D
--- self = Node3D
-
--- test = 0;
-
+local nTest = CreateNode3D("bob")
+local nTestChild = CreateNode3D("bobChild")
 
 function OnInit()
     local node = CreateNode("Created from lua")
+    -- local nTest = CreateNode3D("bob")
+    -- local nTestChild = CreateNode3D("bobChild")
+    -- nTest.AddChild(nTest, nTestChild)
+
     print("billy")
-    local n = CreateNode3D("bob")
-    print("bob")
+    local vec = fmaths.vec3:new(0,0,2)
+    print("vec = (", vec.x,",", vec.y,",", vec.z,")" )
+    nTest.SetWorldPosition(nTest, vec)
+    vec.z = -vec.z
+    print("vec = (", vec.x,",", vec.y,",", vec.z,")" )
+    nTestChild.SetWorldPosition(nTestChild, vec)
+    print("bob" )
+    local pos = nTest:GetWorldPosition()
+    print("Init pos = (", pos.x,",", pos.y,",", pos.z,")" )
+
+    pos = nTestChild:GetWorldPosition()
+    print("InitChild pos = (", pos.x,",", pos.y,",", pos.z,")" )
     
+
 end
 
 function OnUpdate(dt)
+    local vec1 = fmaths.vec3:new(0,0,7)
+    nTest.SetWorldPosition(nTest, vec1)
+
+    local vec = fmaths.vec3:new(0,2,0)
+    -- nTestChild.SetLocalPosition(nTestChild, vec)
+    nTestChild.AddLocalPosition(nTestChild, vec)
+    local pos = nTestChild:GetWorldPosition()
+
+    print("Update pos = (", pos.x,",", pos.y,",", pos.z,")" )
+
     -- local vec = fmaths.vec3.new()
     -- print(vec.x)
 
@@ -31,3 +53,4 @@ function OnUpdate(dt)
     -- end)
     -- test = test + 1;
 end
+
