71fd3d0dba7956dca5141512ebf8a305d500b1c1
Mithaniel V. <mvillardhernandez@gaming.tech>
2026-04-07 10:47:34 +0200

    [REF] Overall clean on Lua .foundry folder

--- res/scripts/.foundry/Physics.d.lua
+++ res/scripts/.foundry/Physics.d.lua
@@ -1,39 +1,39 @@
---@meta
+---@meta
 
---@class physics
+---@class physics
 Physics = {}
 
---@class raycast
---@field impactPos maths.vec3
---@field normal maths.vec3
---@field dist number
---@field node NodeRigidBody
+---@class raycast
+---@field impactPos fmath.vec3
+---@field normal fmath.vec3
+---@field dist number
+---@field node NodeRigidBody
 Physics.raycast = {}
 
 
--- --@param origin maths.vec3
--- --@param direction maths.vec3
--- function Physics:Raycast(origin, direction) end
+--- ---@param origin fmath.vec3
+--- ---@param direction fmath.vec3
+--- function Physics:Raycast(origin, direction) end
 
--- --@param origin maths.vec3
--- --@param direction maths.vec3
--- --@param maxDist number
--- function Physics:Raycast(origin, direction, maxDist) end
+--- ---@param origin fmath.vec3
+--- ---@param direction fmath.vec3
+--- ---@param maxDist number
+--- function Physics:Raycast(origin, direction, maxDist) end
 
---@param origin maths.vec3
---@param direction maths.vec3
---@param maxDist number
---@param mask bitsMask
+---@param origin fmath.vec3
+---@param direction fmath.vec3
+---@param maxDist number
+---@param mask bitsMask
 function Physics:Raycast(origin, direction, maxDist, mask) end
 
 
---@param origin maths.vec3
---@param direction maths.vec3
---@param maxDist number
---@param mask bitsMask
+---@param origin fmath.vec3
+---@param direction fmath.vec3
+---@param maxDist number
+---@param mask bitsMask
 function Physics:RaycastAll(origin, direction, maxDist, mask) end.
 
---@param gravity maths.vec3
+---@param gravity fmath.vec3
 function Physics:SetGravity(gravity) end
 
 function Physics:GetGravity() end
