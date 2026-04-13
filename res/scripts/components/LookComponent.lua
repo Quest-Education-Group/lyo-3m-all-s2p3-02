a1bc0b418a58a695d3336b827cc21f854fe902c8
TheObtey <95309102+TheObtey@users.noreply.github.com>
2026-04-13 18:53:16 +0200

    [ADD] PlayerBase script, with MovementComponent and LookComponent
    Movement component is working, Look component is waiting for the 'icontrol.ReadAsVec2()' function to be fixed.

---
+++ res/scripts/components/LookComponent.lua
@@ -0,0 +1,40 @@
+---@class node
+local self = self
+
+local iMouseSensitivity = 0.002
+local iMinPitch = -89.0
+local iMaxPitch = 89.0
+local iPitch = 0.0
+
+local oRB
+local oCameraRoot
+local oCamera
+
+function self:OnMouseMoveCallback(oContext)
+    local vecMouse = icontrol.ReadAsVec2(oContext) -- TODO: Waiting for Antoine's fix to continue this script
+    print(vecMouse.x, vecMouse.y)
+
+    if not oRB or not oCameraRoot then return end
+
+    -- local vecPlayerUp = oRB:GetLocalUp()
+
+    -- oRB:AddLocalPitch(-vecMouse.x * iMouseSensitivity)
+
+    -- iPitch = iPitch - vecMouse.y * iMouseSensitivity
+    -- iPitch = fmath.Clamp(iPitch, fmath.Rad(fmath.Deg(iMinPitch)), fmath.Rad(fmath.Deg(iMaxPitch)))
+
+    -- oCameraRoot:AddLocalPitch(iPitch)
+end
+
+function self:Setup(oNewRigidBody, oNewCameraRoot, oNewCamera)
+    oRB = oNewRigidBody
+    oCameraRoot = oNewCameraRoot
+    oCamera = oNewCamera
+end
+
+function OnInit()
+    print("LookComponent Initializing")
+end
+
+function OnUpdate(iDelta)
+end
